5c0bf4758e1114ce9960176a3bcf05a2fb2b79b4
Abhijeet Dash <ad@Abhijeets-MacBook-Pro.local>
2024-09-19 13:12:00 +0530

    Released 1.0.3

--- Casks/gpt-widget.rb
+++ Casks/gpt-widget.rb
@@ -1,8 +1,8 @@
 cask 'gpt-widget' do
-  version '1.0.2+1'
-  sha256 '6d9df14af55a22bb41f8eb2ca47957cdc65b7565c0f3239ff65f7e096d451be7'
+  version '1.0.3+1'
+  sha256 'f0dec11316c88c14882e6e431d67e69fdb02d439427df17cd69d805663f343ca'
   
-  url 'https://github.com/AbhijeetDash/gpt_window/releases/download/Beta/gptwidget.1.0.2.dmg'
+  url 'https://github.com/AbhijeetDash/homebrew-gptwindow/releases/download/Beta-1.0.3/gptwidget.1.0.3.dmg'
   name 'GPT Widget'
   desc 'A quick access tool for AI Chatbots'
   homepage 'https://github.com/AbhijeetDash/gpt_window'
